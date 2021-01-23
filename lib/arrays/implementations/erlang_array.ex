2766d0c03144f2258dd9d4904133efea1549b4d4
Eksperimental <eksperimental@autistici.org>
2021-01-22 19:51:03 -0500

    Add @impl to functions

--- lib/arrays/implementations/erlang_array.ex
+++ lib/arrays/implementations/erlang_array.ex
@@ -1,13 +1,17 @@
 defmodule Arrays.Implementations.ErlangArray do
-  alias __MODULE__
-
   @moduledoc """
   Wraps the Erlang `:array` module.
 
   (See: http://erlang.org/doc/man/array.html)
   """
+
+  @behaviour Arrays.Behaviour
+
+  alias __MODULE__
+
   defstruct [:contents]
 
+  @impl Arrays.Behaviour
   # {:default, val} and {:size, num} are forwarded to `:array`
   def empty(options) do
     contents = :array.new([{:fixed, false} | options] ++ [default: nil])
@@ -16,19 +20,16 @@ defmodule Arrays.Implementations.ErlangArray do
 
   use FunLand.Mappable
 
-  def map(array = %ErlangArray{contents: contents}, fun) do
-    new_contents = :array.map(fun, contents)
-    %ErlangArray{array | contents: new_contents}
-  end
-
   use FunLand.Reducible
 
+  @impl FunLand.Reducible
   def reduce(%ErlangArray{contents: contents}, acc, fun) do
     :array.foldr(fun, acc, contents)
   end
 
   @behaviour Access
 
+  @impl Access
   def fetch(%ErlangArray{contents: contents}, index) when index >= 0 do
     if index >= :array.size(contents) do
       :error
@@ -54,6 +55,7 @@ defmodule Arrays.Implementations.ErlangArray do
     end
   end
 
+  @impl Access
   def get_and_update(array = %ErlangArray{contents: contents}, index, function) when index >= 0 do
     if index >= :array.size(contents) do
       {res, _} = function.(nil)
@@ -73,6 +75,7 @@ defmodule Arrays.Implementations.ErlangArray do
     end
   end
 
+  @impl Access
   def get_and_update(array = %ErlangArray{contents: contents}, index, function) when index < 0 do
     if index < -:array.size(contents) do
       {res, _} = function.(nil)
@@ -82,6 +85,7 @@ defmodule Arrays.Implementations.ErlangArray do
     end
   end
 
+  @impl Access
   def pop(array = %ErlangArray{contents: contents}, index) when index >= 0 do
     new_index = index + map_size(contents)
     value = :array.get(contents, new_index)
@@ -96,27 +100,33 @@ defmodule Arrays.Implementations.ErlangArray do
   defimpl Arrays.Protocol do
     alias Arrays.Implementations.ErlangArray
 
+    @impl true
     def size(%ErlangArray{contents: contents}) do
       :array.size(contents)
     end
 
+    @impl true
     def map(array = %ErlangArray{contents: contents}, fun) do
       new_contents = :array.map(fun, contents)
       %ErlangArray{array | contents: new_contents}
     end
 
+    @impl true
     def reduce(%ErlangArray{contents: contents}, acc, fun) do
       :array.foldl(fun, acc, contents)
     end
 
+    @impl true
     def reduce_right(%ErlangArray{contents: contents}, acc, fun) do
       :array.foldr(fun, acc, contents)
     end
 
+    @impl true
     def default(%ErlangArray{contents: contents}) do
       :array.default(contents)
     end
 
+    @impl true
     def get(%ErlangArray{contents: contents}, index) do
       if index < 0 do
         :array.get(contents, index + :array.size(contents))
@@ -125,6 +135,7 @@ defmodule Arrays.Implementations.ErlangArray do
       end
     end
 
+    @impl true
     defdelegate replace(array, index, element), to: __MODULE__, as: :set
 
     def set(array = %ErlangArray{contents: contents}, index, item) do
@@ -138,6 +149,7 @@ defmodule Arrays.Implementations.ErlangArray do
       %ErlangArray{array | contents: new_contents}
     end
 
+    @impl true
     def reset(array = %ErlangArray{contents: contents}, index) do
       new_contents =
         if index < 0 do
@@ -149,16 +161,19 @@ defmodule Arrays.Implementations.ErlangArray do
       %ErlangArray{array | contents: new_contents}
     end
 
+    @impl true
     def append(array = %ErlangArray{contents: contents}, item) do
       new_contents = :array.set(:array.size(contents), item, contents)
       %ErlangArray{array | contents: new_contents}
     end
 
+    @impl true
     def resize(array = %ErlangArray{contents: contents}, new_size) do
       new_contents = :array.resize(contents, new_size)
       %ErlangArray{array | contents: new_contents}
     end
 
+    @impl true
     def extract(array = %ErlangArray{contents: contents}) do
       case :array.size(contents) do
         0 ->
@@ -173,6 +188,7 @@ defmodule Arrays.Implementations.ErlangArray do
       end
     end
 
+    @impl true
     def to_list(%ErlangArray{contents: contents}) do
       :array.to_list(contents)
     end
