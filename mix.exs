80f0c25d107adfb58b456707b8e1aef8c58c909b
Marten/Qqwy <w-m@wmcode.nl>
2021-09-04 04:48:24 +0200

    Revert

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Arrays.Mixfile do
     [
       app: :arrays,
       version: "1.0.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
