1ef50b7eb8a546840292c1d135607dbb78e61736
Marten/Qqwy <w-m@wmcode.nl>
2021-09-04 04:42:47 +0200

    ... by also lowering the Elixir version in the mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Arrays.Mixfile do
     [
       app: :arrays,
       version: "1.0.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
