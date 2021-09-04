8e574c662b931ed7dc5b880202c341b7d95fcdf0
Marten/Qqwy <w-m@wmcode.nl>
2021-09-04 05:28:57 +0200

    Elixir v ~> 1.7 it is.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Arrays.Mixfile do
     [
       app: :arrays,
       version: "1.0.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
